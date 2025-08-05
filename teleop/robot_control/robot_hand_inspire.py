0ab3074c134f724f637e86871efa2f33255b39dc
FranRFH <fran@robotsxhumanity.com>
2025-08-05 13:45:04 +0000

    Agrego modificacion para teleoperar el UNitree G1 con las inspire hand

--- teleop/robot_control/robot_hand_inspire.py
+++ teleop/robot_control/robot_hand_inspire.py
@@ -27,10 +27,6 @@ class Inspire_Controller:
             self.hand_retargeting = HandRetargeting(HandType.INSPIRE_HAND)
         else:
             self.hand_retargeting = HandRetargeting(HandType.INSPIRE_HAND_Unit_Test)
-
-        if self.simulation_mode:
-            ChannelFactoryInitialize(1)
-        else:
             ChannelFactoryInitialize(0)
 
         # initialize handcmd publisher and handstate subscriber
